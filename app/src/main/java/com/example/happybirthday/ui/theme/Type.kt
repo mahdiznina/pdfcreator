6e39260122230e1d931e381d3ca993ac41257bd3
ZNINA mahdi <152227691+mahdiznina@users.noreply.github.com>
2024-10-07 12:24:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/ui/theme/Type.kt
@@ -0,0 +1,8 @@
+package com.example.happybirthday.ui.theme
+
+import androidx.compose.material3.Typography
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    // Customize typography here
+)
